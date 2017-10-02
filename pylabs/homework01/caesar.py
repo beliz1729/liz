322f457505bcfc0b82fe59f0ace942f393a426c0
Elizabeth <elizabethb@niuitmo.ru>
2017-10-02 22:51:16 +0300

    Реализованы функции шифрования и дешифрования

---
+++ pylabs/homework01/caesar.py
@@ -0,0 +1,31 @@
+def encrypt_caesar(plaintext):
+    text = str(plaintext)
+    cyphertext = ""
+    alp = 26
+    sp = 3
+    for i in range(len(text)):
+        k = text[i]
+        if (97 <= ord(k)+sp <= 122) or (65 <= ord(k)+sp <= 90): #cимволы от a до z и от A до Z
+            cyphertext += chr(ord(k)+sp) #символ + сдвиг
+        else:
+            cyphertext += chr(ord(k)-alp+sp)
+    return cyphertext
+print ('put your text here')
+plaintext = input()
+print ('result:', encrypt_caesar(plaintext))
+
+def decrypt_caesar(cyphertext):
+    text = str(cyphertext)
+    plaintext = ""
+    alp = 26
+    sp = 3
+    for i in range(len(cyphertext)):
+        k = cyphertext[i]
+        if (97 <= ord(k)-sp <= 122) or (65 <= ord(k)-sp <= 90):
+            plaintext += chr(ord(k)-sp)
+        else:
+            plaintext += chr(ord(k)+alp-sp)
+    return plaintext
+print ('put your text here')
+cyphertext = input()
+print ('result:', decrypt_caesar(cyphertext))
