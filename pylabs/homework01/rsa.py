58624746bd4eab82acd88d7ffa55c6b38813403b
Elizabeth <elizabethb@niuitmo.ru>
2017-10-03 11:41:45 +0300

    Реализована функция encrypt и decrypt

--- pylabs/homework01/rsa.py
+++ pylabs/homework01/rsa.py
@@ -22,34 +22,41 @@ def is_prime(n):
     return number
 
 
-def gcd(a, b):
-    """
-    Euclid's algorithm for determining the greatest common divisor.
-
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
-    # PUT YOUR CODE HERE
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-    print (a+b)
-
+def ext_gcd(e, phi):
+    if phi == 0:
+        return e, 1, 0
+    else:
+        d, x, y = ext_gcd(phi, e % phi)
+    return d, y, x - y * (e // phi)
 
 def multiplicative_inverse(e, phi):
     """
     Euclid's extended algorithm for finding the multiplicative
     inverse of two numbers.
 
-    >>> multiplicative_inverse(7, 40)
+    »> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    d = ext_gcd(e, phi)
+    result = d[1] % phi
+    return result
+
+def gcd(a, b):
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
 
 
 def generate_keypair(p, q):
@@ -59,19 +66,21 @@ def generate_keypair(p, q):
         raise ValueError('p and q cannot be equal')
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p-1)*(q-1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
+    
 
     # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
+        
 
     # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
@@ -80,25 +89,21 @@ def generate_keypair(p, q):
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
-
 def encrypt(pk, plaintext):
-    # Unpack the key into it's components
+    #Unpack the key into it's components
     key, n = pk
-    # Convert each letter in the plaintext to numbers based on
-    # the character using a^b mod m
+    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
+    #Return the array of bytes
     return cipher
 
-
 def decrypt(pk, ciphertext):
-    # Unpack the key into its components
+    #Unpack the key into its components
     key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    #Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
+    #Return the array of bytes as a string
     return ''.join(plain)
-
 
 if __name__ == '__main__':
     print("RSA Encrypter/ Decrypter")
@@ -106,11 +111,12 @@ if __name__ == '__main__':
     q = int(input("Enter another prime number (Not one you entered above): "))
     print("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
-    print("Your public key is ", public, " and your private key is ", private)
+    print("Your public key is ", public ," and your private key is ", private)
     message = input("Enter a message to encrypt with your private key: ")
     encrypted_msg = encrypt(private, message)
     print("Your encrypted message is: ")
     print(''.join(map(lambda x: str(x), encrypted_msg)))
-    print("Decrypting message with public key ", public, " . . .")
+    print("Decrypting message with public key ", public ," . . .")
     print("Your message is:")
     print(decrypt(public, encrypted_msg))
+    
