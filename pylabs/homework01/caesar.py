82bbdaa5a02d20ae56dd93b1db13c201168167ce
Elizabeth <elizabethb@niuitmo.ru>
2017-10-07 20:05:29 +0300

    Исправлено

--- pylabs/homework01/caesar.py
+++ pylabs/homework01/caesar.py
@@ -12,10 +12,11 @@ def encrypt_caesar(plaintext):
     sp = 3
     for i in range(len(str(plaintext))):
         k = str(plaintext[i])
+        m = plaintext[i]
         if (97 <= ord(k)+sp <= 122) or (65 <= ord(k)+sp <= 90):
             cyphertext += chr(ord(k)+sp)
-        elif (122 < (ord(k) + sp) <= 125) or (90 < (ord(k) + sp) <= 93):
-            cyphertext += chr(ord(k)-alp+sp)
+        elif (122 < (ord(m) + sp) <= 125) or (90 < (ord(m) + sp) <= 93):
+            cyphertext += chr(ord(m)-alp+sp)
         else:
             cyphertext += plaintext
     return cyphertext
@@ -35,10 +36,11 @@ def decrypt_caesar(cyphertext):
     sp = 3
     for i in range(len(str(cyphertext))):
         k = str(cyphertext[i])
+        m = cyphertext[i]
         if (97 <= ord(k)-sp <= 122) or (65 <= ord(k)-sp <= 90):
             plaintext += chr(ord(k)-sp)
-        elif (94 <= (ord(k)-sp) < 97) or (62 <= (ord(k)-sp) < 65):
-            plaintext += chr(ord(k)+alp-sp)
+        elif (94 <= (ord(m)-sp) < 97) or (62 <= (ord(m)-sp) < 65):
+            plaintext += chr(ord(m)+alp-sp)
         else:
             plaintext += cyphertext
     return plaintext
